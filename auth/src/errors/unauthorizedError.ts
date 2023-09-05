ecdea574df7b6e3b25b0b7fa2f338b6ea83a2a4a
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-09-05 23:20:01 +0700

    add unauthorized error and middleware for require auth

---
+++ auth/src/errors/unauthorizedError.ts
@@ -0,0 +1,15 @@
+import { CustomError } from './customError';
+
+export class UnauthorizedError extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super('Unauthorized');
+
+    Object.setPrototypeOf(this, UnauthorizedError.prototype);
+  }
+
+  serializeErrors(): { message: string; field?: string | undefined }[] {
+    return [{ message: 'Not authorized' }];
+  }
+}
