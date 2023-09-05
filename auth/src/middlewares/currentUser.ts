23f5711ce5d8a23b50f8cd0ee53592fcc9d640c7
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-09-05 23:04:23 +0700

    create middleware for current user

---
+++ auth/src/middlewares/currentUser.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface userPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: userPayload;
+    }
+  }
+}
+
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session?.jwt) return next();
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as userPayload;
+    req.currentUser = payload;
+  } catch (err) {}
+
+  next();
+};
