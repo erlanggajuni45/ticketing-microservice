8eb20795c5aaccd09d0464320fbbd14cb222642c
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-09-06 20:14:44 +0700

    refactor index and add dependencies for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/currentUser';
+import { signinRouter } from './routes/signIn';
+import { signoutRouter } from './routes/signOut';
+import { signupRouter } from './routes/signUp';
+import { errorHandler } from './middlewares/errorHandling';
+import { NotFoundError } from './errors/notFoundError';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(express.json());
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    secure: true,
+    signed: false,
+  })
+);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export default app;
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,34 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { currentUserRouter } from './routes/currentUser';
-import { signinRouter } from './routes/signIn';
-import { signoutRouter } from './routes/signOut';
-import { signupRouter } from './routes/signUp';
-import { errorHandler } from './middlewares/errorHandling';
-import { NotFoundError } from './errors/notFoundError';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-const app = express();
-
-app.use(express.json());
-app.set('trust proxy', true);
-app.use(
-  cookieSession({
-    secure: true,
-    signed: false,
-  })
-);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import app from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
