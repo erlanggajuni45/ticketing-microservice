f3ee006f4fa7128fac283f7b5a050429aed2fe40
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-09-09 22:26:52 +0700

    handle error form sign up client

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,20 +1,52 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    setErrors([]);
+    try {
+      const response = await axios.post('/api/users/signup', { email, password });
+      console.log(response);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className='form-group'>
         <label>Email</label>
         <input
           className='form-control'
-          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.find((err) => err.field === 'email') ? (
+          <p className='text-danger'>{errors.find((err) => err.field === 'email').message}</p>
+        ) : (
+          ''
+        )}
       </div>
       <div className='form-group'>
         <label>Password</label>
         <input
           className='form-control'
           type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
+        {errors.find((err) => err.field === 'password') ? (
+          <p className='text-danger'>{errors.find((err) => err.field === 'password').message}</p>
+        ) : (
+          ''
+        )}
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>
